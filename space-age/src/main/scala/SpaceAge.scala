c085be12381bafa983c1e8fec38a2d29cd41024e
gardncl <gardncl@gmail.com>
2017-10-01 18:27:55 -0400

    space-age:2:cleaner implementation

--- space-age/src/main/scala/SpaceAge.scala
+++ space-age/src/main/scala/SpaceAge.scala
@@ -1,12 +1,14 @@
 object SpaceAge {
-  def onEarth(age: Double): Double = age / secondsInYear(365.25)
-  def onMercury(age: Double): Double = age / secondsInYear(87.97)
-  def onVenus(age: Double): Double = age / secondsInYear(224.7)
-  def onMars(age: Double): Double = age / secondsInYear(1.88)
-  def onJupiter(age: Double): Double = age / secondsInYear(11.86)
-  def onSaturn(age: Double): Double = age / secondsInYear(29.46)
-  def onUranus(age: Double): Double = age / secondsInYear(84.01)
-  def onNeptune(age: Double): Double = age / secondsInYear(164.79)
+  def onMercury(age: Double): Double = earthAge(age, .24)
+  def onVenus(age: Double): Double = earthAge(age, 0.62)
+  def onEarth(age: Double): Double = earthAge(age, 1)
+  def onMars(age: Double): Double = earthAge(age, 1.88)
+  def onJupiter(age: Double): Double = earthAge(age, 11.86)
+  def onSaturn(age: Double): Double = earthAge(age, 29.46)
+  def onUranus(age: Double): Double = earthAge(age, 84.01)
+  def onNeptune(age: Double): Double = earthAge(age, 164.79)
 
-  def secondsInYear(daysInYear: Double): Double = 24 * 60 * 60 * daysInYear
+  private val earthOrbit: Double = 365.25
+  private def earthAge(age: Double, seconds: Double): Double = age / secondsInYear(seconds)
+  private def secondsInYear(orbitComparedToEarth: Double): Double = 24 * 60 * 60 * (orbitComparedToEarth * earthOrbit)
 }
