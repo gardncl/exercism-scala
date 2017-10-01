131a27cbe493adf979d4f6d7ea0a8da83e87650c
gardncl <gardncl@gmail.com>
2017-10-01 16:27:43 -0400

    leap:4:remove if else

--- leap/src/main/scala/Leap.scala
+++ leap/src/main/scala/Leap.scala
@@ -1,7 +1,6 @@
 object Leap {
   def leapYear(year: Int): Boolean = {
-    if (divisible(4) && (!divisible(100) || divisible(400))) true
-    else false
+    divisible(4) && (!divisible(100) || divisible(400))
 
     def divisible(n: Int): Boolean = year % 4 == 0
   }
