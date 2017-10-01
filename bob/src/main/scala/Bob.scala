6a302d911bc92c0c3235990527bd50436c1078a3
gardncl <gardncl@gmail.com>
2017-10-01 17:43:32 -0400

    bob:1:can't use case statements with regex?

--- bob/src/main/scala/Bob.scala
+++ bob/src/main/scala/Bob.scala
@@ -1,3 +1,16 @@
 object Bob {
-  def response(statement: String): String = ???
+  def response(statement: String): String = {
+    if (statement.matches(shouting))
+      "Whoa, chill out!"
+    else if (statement.matches(question))
+      "Sure."
+    else if (statement.matches(address))
+      "Fine. Be that way!"
+    else
+      "Whatever."
+  }
+
+  val shouting = "(?=.*[A-Z])[^a-z]*"
+  val question = ".*[?]\\s*"
+  val address = "\\s*"
 }
